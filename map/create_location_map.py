aa9835ffe5e22f2320f8c414a942b2aebfb11128
a-baur <alexba.baur@protonmail.com>
2022-07-12 18:30:20 +0200

    Heatmap

--- map/create_location_map.py
+++ map/create_location_map.py
@@ -1,22 +1,25 @@
 import folium
 from out import venue_locations
 import webbrowser
-from folium.plugins import MarkerCluster
+from folium.plugins import MarkerCluster, HeatMap
 
 if __name__ == "__main__":
-    latitudes_uhh = [i[0] for i in venue_locations.uhh_coord]
-    longitudes_uhh = [i[1] for i in venue_locations.uhh_coord]
-    latitudes_tuhh = [i[0] for i in venue_locations.tuhh_coord]
-    longitudes_tuhh = [i[1] for i in venue_locations.tuhh_coord]
-    latitudes_haw = [i[0] for i in venue_locations.haw_coord]
-    longitudes_haw = [i[1] for i in venue_locations.haw_coord]
+    latitudes_uhh = [i[0] for i in venue_locations.uhh_coords]
+    longitudes_uhh = [i[1] for i in venue_locations.uhh_coords]
+    latitudes_tuhh = [i[0] for i in venue_locations.tuhh_coords]
+    longitudes_tuhh = [i[1] for i in venue_locations.tuhh_coords]
+    latitudes_haw = [i[0] for i in venue_locations.haw_coords]
+    longitudes_haw = [i[1] for i in venue_locations.haw_coords]
     folium_map = folium.Map(location=[20, 0], max_bounds=True, zoom_start=2, tiles='CartoDB positron')
     uhh_layer = folium.FeatureGroup(name="University of Hamburg")
     uhh_cluster = MarkerCluster(locations=list(zip(latitudes_uhh, longitudes_uhh)), popups=venue_locations.uhh_ven).add_to(uhh_layer)
+    HeatMap(venue_locations.uhh_coords).add_to(uhh_layer)
     tuhh_layer = folium.FeatureGroup(name="Hamburg University of Technology")
     tuhh_cluster = MarkerCluster(locations=list(zip(latitudes_tuhh, longitudes_tuhh)), popups=venue_locations.tuhh_ven).add_to(tuhh_layer)
+    HeatMap(venue_locations.tuhh_coords).add_to(tuhh_layer)
     haw_layer = folium.FeatureGroup(name="Hamburg University of Applied Sciences")
     haw_cluster = MarkerCluster(locations=list(zip(latitudes_haw, longitudes_haw)), popups=venue_locations.haw_ven).add_to(haw_layer)
+    HeatMap(venue_locations.haw_coords).add_to(haw_layer)
     folium_map.add_child(uhh_layer)
     folium_map.add_child(tuhh_layer)
     folium_map.add_child(haw_layer)
